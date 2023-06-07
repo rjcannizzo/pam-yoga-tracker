5cbb051343522abe36d72fc3b92ae217e14509dd
rjcannizzo <rc.info@protonmail.com>
2023-06-07 09:14:55 -0500

    started with outline and did some experimenting with Session State and editor widget.

---
+++ editor.py
@@ -0,0 +1,18 @@
+""""
+Test the data editor widget allows you to edit dataframes and many other data structures in a table-like UI.
+st.data_editor
+"""
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame(
+    [
+       {"command": "st.selectbox", "rating": 4, "is_widget": True},
+       {"command": "st.balloons", "rating": 5, "is_widget": False},
+       {"command": "st.time_input", "rating": 3, "is_widget": True},
+   ]
+)
+edited_df = st.data_editor(df)
+
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
