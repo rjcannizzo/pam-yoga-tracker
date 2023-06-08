e2fa66a3c4bdad781c7b07e958ac43fdabf0bb59
rjcannizzo <rc.info@protonmail.com>
2023-06-08 07:16:26 -0500

    added modules folder with exceptions.py, added main function, ,cleaned up code

--- Yoga-Tracker.py
+++ Yoga-Tracker.py
@@ -4,24 +4,17 @@ import pandas as pd
 import pymongo
 import dotenv
 
-class YogaException(Exception):
-    pass
+from modules.exceptions import YogaException
 
 dotenv.load_dotenv('.env')
-DATABASE = "PAM"
-MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
-URI = f"mongodb+srv://rc:{MONGO_PASSWORD}@apps.sdrf5qb.mongodb.net/{DATABASE}"
-
-st.header('Yoga Teaching Tracker')
-message_area = st.empty()
 
 @st.cache_resource
-def get_mongo_client():    
+def get_mongo_client():
+    DATABASE = "PAM"
+    MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
+    URI = f"mongodb+srv://rc:{MONGO_PASSWORD}@apps.sdrf5qb.mongodb.net/{DATABASE}"   
     client = pymongo.MongoClient(URI)    
     return client
-
-if 'mongo_client' not in st.session_state:
-    st.session_state['mongo_client'] = get_mongo_client()
 
 def get_data(query, projection) -> pymongo.cursor.Cursor:
     """Retrieve data from mongo database. Cursor returns dictionaries during iteration."""
@@ -58,10 +51,10 @@ def get_summary_data() -> dict:
     result = list(aggregate_query(pipeline))
     try:
         data = result[0]
-    except Exception as e:
-        st.error(e)
-        return
+    except IndexError as e:        
+        raise YogaException(e)
     return data
+    
     
 def display_summary_data(data):
     """"""
@@ -71,7 +64,22 @@ def display_summary_data(data):
     st.write(total_minutes)
     st.write(total_pay)
     st.write(sessions)
+
+
+def main():
+    st.header('Yoga Teaching Tracker')
+    message_area = st.empty()
     
-summary_data = get_summary_data()
-display_summary_data(summary_data)
-get_all_records()
+    if 'mongo_client' not in st.session_state:
+        st.session_state['mongo_client'] = get_mongo_client()
+    try:   
+        summary_data = get_summary_data()
+        display_summary_data(summary_data)
+    except YogaException as e:
+        st.error(f"The app has encountered an error: {e}")
+
+    get_all_records()
+    message_area.success('Success!!')
+    st.balloons()
+    
+main()
---
+++ modules/exceptions.py
@@ -0,0 +1,2 @@
+class YogaException(Exception):
+    pass
