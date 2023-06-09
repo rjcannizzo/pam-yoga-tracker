78e32dde49c90774625f3608bf5370241570529f
rjcannizzo <rc.info@protonmail.com>
2023-06-09 08:21:28 -0500

    completed process_new_record and insert_record.

--- Yoga-Tracker.py
+++ Yoga-Tracker.py
@@ -15,6 +15,35 @@ def aggregate_query(pipeline):
     return collection.aggregate(pipeline)
 
 
+def display_new_record_form():
+    """Displays the form used to enter new records."""
+    with st.form("add_record_form"):
+        st.header("Add a New Record")
+        st.date_input("Date", value=None, min_value=None, max_value=None, key="date_new", help=None, on_change=None, args=None, kwargs=None)
+        st.number_input("Minutes", min_value=1, max_value=600, value=75, step=None, format=None, key="minutes_new", help=None, on_change=None)
+        st.number_input("Pay", min_value=0, max_value=1000, value=30, step=None, format=None, key="pay_new", help=None, on_change=None)
+        st.selectbox("Studio", ["Mindful Motion"], index=0, key="studio_new", help=None, on_change=None)
+        st.selectbox("Type", ["Restorative", "Test"], index=0, key="class_new", help=None, on_change=None)
+        st.number_input("Students", min_value=1, max_value=1000, value=2, step=None, format=None, key="students_new", help=None, on_change=None)        
+        st.form_submit_button("Submit", on_click=process_new_record)        
+            
+            
+def process_new_record():
+    """Collect form information and insert a new record into the database"""    
+    minutes = st.session_state.minutes_new
+    pay = st.session_state.pay_new
+    studio = st.session_state.studio_new
+    class_type = st.session_state.class_new
+    students = st.session_state.students_new 
+    date = datetime.datetime.combine(st.session_state.date_new, datetime.time(hour=0, minute=0, second=0))
+    data = {'date': date, 'duration': minutes, 'studio': studio, "type": class_type, 'pay': pay, 'students': students}
+    _id = insert_record(data)
+    if _id:
+        display_message('Success! New record added...', 'info')
+        st.balloons()        
+    else:
+        display_message('Error inserting record', 'error')
+
 def display_summary_data(data):
     """Disply summary of classes, hours, income and students in a dataframe"""
     total_minutes = data.get('total_minutes', 0)
@@ -71,11 +100,15 @@ def get_summary_data() -> dict:
 
 def insert_record(data: dict):
     """""" 
-    # data ={'date': datetime.datetime.now(), 'duration': 75, 'studio': "Mindful Motion", "type": 'Test', 'pay': 30, students: 101}
+    # data = {'date': datetime.datetime.now(), 'duration': 75, 'studio': "Mindful Motion", "type": 'Test', 'pay': 30, students: 101}
     client = st.session_state['mongo_client']
-    collection = client.pam.yoga    
-    id = collection.insert_one(data).inserted_id    
-    return id
+    collection = client.pam.yoga
+    try:    
+        id = collection.insert_one(data).inserted_id
+        return id
+    except Exception as e:
+        print(e)  
+
 
 
 def display_message(msg: str, msg_type: str):
@@ -85,24 +118,7 @@ def display_message(msg: str, msg_type: str):
     """
     message_area = st.session_state['message_area']
     method = getattr(message_area, msg_type)
-    method(msg)
-    
-    
-def display_new_record_form():
-    """Displays the form used to enter new records."""
-    with st.form("add_record_form"):
-        st.header("Add a New Record")
-        st.date_input("Date", value=None, min_value=None, max_value=None, key="date_new", help=None, on_change=None, args=None, kwargs=None)
-        st.number_input("Minutes", min_value=1, max_value=600, value=75, step=None, format=None, key="minutes_new", help=None, on_change=None)
-        st.number_input("Pay", min_value=0, max_value=1000, value=30, step=None, format=None, key="pay_new", help=None, on_change=None)
-        st.selectbox("Studio", ["Mindful Motion"], index=0, key="studio_new", help=None, on_change=None)
-        st.selectbox("Type", ["Restorative"], index=0, key="type_new", help=None, on_change=None)
-        st.number_input("Students", min_value=1, max_value=1000, value=2, step=None, format=None, key="students_new", help=None, on_change=None)
-        
-        submitted = st.form_submit_button("Submit", on_click=None)
-        if submitted:
-            st.write("Submitted!")
-
+    method(msg) 
     
 
 def main():
@@ -119,9 +135,9 @@ def main():
     except YogaException as e:
         st.error(f"The app has encountered an error: {e}")
 
-    all_df = get_all_records_as_dataframe()
-    # insert_record()
-    # display_message('testing....', 'info')
     display_new_record_form()
+    # all_df = get_all_records_as_dataframe()
+    display_message('testing....', 'info')
+    
     
 main()
