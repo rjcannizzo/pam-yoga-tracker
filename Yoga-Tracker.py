9b29995544a1e041f60745c262c22b1fdcd6e484
rjcannizzo <rc.info@protonmail.com>
2023-06-09 06:31:57 -0500

    added display_message() to show user info using st.info, st.success, etc.

--- Yoga-Tracker.py
+++ Yoga-Tracker.py
@@ -8,13 +8,31 @@ import dotenv
 from modules.exceptions import YogaException
 
 
-@st.cache_resource
-def get_mongo_client():
-    DATABASE = "PAM"
-    MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
-    URI = f"mongodb+srv://rc:{MONGO_PASSWORD}@apps.sdrf5qb.mongodb.net/{DATABASE}"   
-    client = pymongo.MongoClient(URI)    
-    return client
+def aggregate_query(pipeline):
+    """Runs an aggregation query"""
+    client = st.session_state['mongo_client']
+    collection = client.pam.yoga    
+    return collection.aggregate(pipeline)
+
+
+def display_summary_data(data):
+    """Disply summary of classes, hours, income and students in a dataframe"""
+    total_minutes = data.get('total_minutes', 0)
+    total_pay = data.get('total_pay', 0)
+    sessions = data.get('sessions', 0)
+    students = data.get('total_students', 0)
+    data = [
+        {"Classes": sessions, "Hours": f"{total_minutes / 60:.2f}", "Income": f"${total_pay:.2f}", "Students": students}        
+    ]
+    df = pd.DataFrame(data, index=['Totals'])
+    st.header("Summary")
+    df
+
+
+def get_all_records_as_dataframe() -> pd.DataFrame:
+    """Revise to return all records and display in another function""" 
+    records = get_data(query={}, projection={'_id': 0})    
+    return pd.DataFrame(records)
 
 
 def get_data(query, projection) -> pymongo.cursor.Cursor:
@@ -22,20 +40,14 @@ def get_data(query, projection) -> pymongo.cursor.Cursor:
     client = st.session_state['mongo_client']
     collection = client.pam.yoga
     items = collection.find(query, projection)    
-    return items
+    return items   
 
-
-def get_all_records_as_dataframe() -> pd.DataFrame:
-    """Revise to return all records and display in another function""" 
-    records = get_data(query={}, projection={'_id': 0})    
-    return pd.DataFrame(records)    
-    
-    
-def aggregate_query(pipeline):
-    """Runs an aggregation query"""
-    client = st.session_state['mongo_client']
-    collection = client.pam.yoga    
-    return collection.aggregate(pipeline)
+@st.cache_resource
+def get_mongo_client():
+    DATABASE = "PAM"
+    MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
+    URI = f"mongodb+srv://rc:{MONGO_PASSWORD}@apps.sdrf5qb.mongodb.net/{DATABASE}"   
+    return pymongo.MongoClient(URI)
     
     
 def get_summary_data() -> dict:
@@ -58,35 +70,30 @@ def get_summary_data() -> dict:
         raise YogaException(e)
     return data
     
-    
-def display_summary_data(data):
-    """"""
-    total_minutes = data.get('total_minutes', 0)
-    total_pay = data.get('total_pay', 0)
-    sessions = data.get('sessions', 0)
-    students = data.get('total_students', 0)
-    data = [
-        {"Classes": sessions, "Hours": f"{total_minutes / 60:.2f}", "Income": f"${total_pay:.2f}", "Students": students}        
-    ]
-    df = pd.DataFrame(data, index=['Totals'])
-    st.header("Summary")
-    df
-    
 
 def insert_record(data: dict):
     """""" 
-    # data ={'date': datetime.datetime.now(), 'duration': 75, 'studio': "Mindful Motion", "type": 'Test', 'pay': 30}
+    # data ={'date': datetime.datetime.now(), 'duration': 75, 'studio': "Mindful Motion", "type": 'Test', 'pay': 30, students: 101}
     client = st.session_state['mongo_client']
     collection = client.pam.yoga    
     id = collection.insert_one(data).inserted_id
     # st.balloons()
     return id
 
+def display_message(msg: str, msg_type: str):
+    """
+    Display a message at the top of the app in the 'message_area' declared in main() using st.empty()
+    msg_type: error, info, success, warning
+    """
+    message_area = st.session_state['message_area']
+    method = getattr(message_area, msg_type)
+    method(msg)
 
 def main():
     dotenv.load_dotenv('.env')
     st.title('Yoga Teaching Tracker 🙏')
     message_area = st.empty()
+    st.session_state['message_area'] = message_area
     
     if 'mongo_client' not in st.session_state:
         st.session_state['mongo_client'] = get_mongo_client()
@@ -98,6 +105,6 @@ def main():
 
     all_df = get_all_records_as_dataframe()
     # insert_record()
-    
+    display_message('testing....', 'info')
     
 main()
